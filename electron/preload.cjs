cc865c7e6758c20fbbd536a5fed89fd4270d7bf6
Yasser Diab <yasserdiabhassan@gmail.com>
2026-06-14 23:10:11 +0300

    Delete electron directory

--- electron/preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('priceOfferDesktop', {
-  apiBase: `http://127.0.0.1:${process.env.PRICE_OFFER_PORT || 4181}`,
-  platform: process.platform,
-  openExternal: (url) => ipcRenderer.invoke('open-external-url', url),
-});
